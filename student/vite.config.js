69587361e82ea2361cfd4822153cb7d88b506470
長塚裕也 <ny410144@gmail.com>
2024-10-11 11:20:29 +0900

    viteを使ってCSSとJSを読み込む

--- student/vite.config.js
+++ student/vite.config.js
@@ -11,4 +11,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5174,
+        strictPort: true,
+        hmr: {
+          host: 'localhost',
+          port: 5174,
+          protocol: 'ws',
+        },
+    },
 });
