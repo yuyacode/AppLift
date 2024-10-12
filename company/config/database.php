22fe07a29713a0428883b71cf2cab0b685cc55ea
長塚裕也 <ny410144@gmail.com>
2024-10-12 15:00:25 +0900

    common DBへの接続情報追加

--- company/config/database.php
+++ company/config/database.php
@@ -82,6 +82,26 @@ return [
             ]) : [],
         ],
 
+        'common' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB3_HOST', '127.0.0.1'),
+            'port' => env('DB3_PORT', '3306'),
+            'database' => env('DB3_DATABASE', 'forge'),
+            'username' => env('DB3_USERNAME', 'forge'),
+            'password' => env('DB3_PASSWORD', ''),
+            'unix_socket' => env('DB3_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
