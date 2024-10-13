0e7bb02782ba070cee448c065f024ca179d315c6
長塚裕也 <ny410144@gmail.com>
2024-10-13 12:26:41 +0900

    company_infosテーブルのモデル、マイグレーションファイル、ファクトリーファイル用意

---
+++ company/app/Models/CompanyInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CompanyInfo extends Model
+{
+    /** @use HasFactory<\Database\Factories\CompanyInfoFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $connection = 'common';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'HP'
+    ];
+}
