cfe06f1477f54fc3fa7ba0c5e472452d7cc9c514
長塚裕也 <ny410144@gmail.com>
2024-10-27 09:38:40 +0900

    サブアカウント追加時に、そのアカウントに紐づくレビューと回答を空で作成する

--- company/app/Http/Controllers/UserController.php
+++ company/app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
+use App\Models\ReviewAnswer;
+use App\Models\ReviewItem;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -24,13 +27,32 @@ class UserController extends Controller
             'password' => ['required', Rules\Password::defaults()],
         ]);
 
-        User::create([
+        $user = User::create([
             'company_info_id' => $request->user()->companyInfo->id,
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'is_master' => 0
         ]);
+
+        $review = Review::create([
+            'company_user_id' => $user->id,
+            'status' => 0,
+        ]);
+
+        $default_review_item_ids = ReviewItem::where('is_default', 1)->pluck('id');
+
+        $review_answers = array();
+        $now = now();
+        foreach ($default_review_item_ids as $id) :
+            array_push($review_answers, [
+                'review_id' => $review->id,
+                'review_item_id' => $id,
+                'created_at' => $now
+            ]);
+        endforeach;
+
+        ReviewAnswer::insert($review_answers);
 
         return to_route('company_info.index')->with('status_company-member', 'メンバーを追加しました');
     }
