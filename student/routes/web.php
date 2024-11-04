83d9e4e2f01e13cc91a27c2f7487b82ea15173a6
長塚裕也 <ny410144@gmail.com>
2024-11-04 14:54:39 +0900

    プロフィールに対するルーティングをグループ化

--- student/routes/web.php
+++ student/routes/web.php
@@ -4,20 +4,21 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('student')->group(function () {
-
-    Route::get('/', function () {
-        return view('welcome');
-    });
-
     Route::middleware('auth')->group(function () {
 
         Route::get('/dashboard', function () {
             return view('dashboard');
         })->name('dashboard');
 
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::prefix('profile')
+            ->name('profile.')
+            ->controller(ProfileController::class)
+            ->group(function () {
+                Route::get('/', 'edit')->name('edit');
+                Route::patch('/', 'update')->name('update');
+                Route::delete('/', 'destroy')->name('destroy');
+            }
+        );
     });
 
     require __DIR__.'/auth.php';
