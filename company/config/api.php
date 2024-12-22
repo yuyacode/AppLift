14cc791466ac319ab254e0b699085d5126370517
長塚裕也 <ny410144@gmail.com>
2024-12-23 00:27:24 +0900

    フロントで利用されるAPIベースURLを環境変数に追加

--- company/config/api.php
+++ company/config/api.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'message_api_base_url_backend' => env('MESSAGE_API_BASE_URL_BACKEND', ''),
     'message_api_key' => env('MESSAGE_API_KEY', ''),
+    'message_api_base_url_frontend' => env('MESSAGE_API_BASE_URL_FRONTEND', ''),
+    'message_api_base_url_backend' => env('MESSAGE_API_BASE_URL_BACKEND', ''),
 ];
