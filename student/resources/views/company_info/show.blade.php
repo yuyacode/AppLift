5b3eee72d1621cdf0cff74404a2d7226e8c8143d
長塚裕也 <ny410144@gmail.com>
2025-01-13 13:21:58 +0900

    情報量が多く窮屈感を感じたため、表示する情報を減らしつつ、余白を広げる

--- student/resources/views/company_info/show.blade.php
+++ student/resources/views/company_info/show.blade.php
@@ -7,29 +7,28 @@
                     <p class="block font-medium text-sm text-gray-700 mt8">所在地：{{ $company_info->address }}</p>
                     <p class="block font-medium text-sm text-gray-700 mt8">ホームページ：{{ $company_info->homepage }}</p>
                 </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg space-y-6">
                 <p>メンバー</p>
-                <div class="flex_custom wrap gap2per flex-start">
+                <div class="flex_custom wrap gap4per flex-start">
                     @foreach ($members as $member)
-                        <div class="w32per mb24 border-gray rounded-md shadow-sm border-box pointer">
+                        <div class="w22per mb24 border-gray rounded-md shadow-sm border-box pointer">
                             <a href="{{ route('company_info.member', ['company_info' => $member->company_info_id, 'company_user' => $member->id]) }}">
                                 <div class="p8">
                                     <p>{{ $member->name }}</p>
-                                    <p class="block font-medium text-sm text-gray-700 mt8">部署、部門：{{ $member->department }}</p>
-                                    <p class="block font-medium text-sm text-gray-700 mt8">職種、職業：{{ $member->occupation }}</p>
-                                    <p class="block font-medium text-sm text-gray-700 mt8">役職、ポジション：{{ $member->position }}</p>
-                                    <p class="block font-medium text-sm text-gray-700 mt8">入社時期：{{ $member->join_date }}</p>
+                                    <p class="block font-medium text-sm text-gray-700 mt8">{{ $member->department }}、{{ $member->occupation }}、{{ $member->position }}</p>
                                 </div>
                                 <div class="p8 bt-gray">
                                     <p class="introduction font-medium text-sm text-gray-700">{{ $member->introduction }}</p>
                                 </div>
-                                <div class="p8 bt-gray">
+                                <!-- <div class="p8 bt-gray">
                                     <p class="font-medium text-sm text-gray-700">レビュー</p>
                                     @if($member->review->status)
                                         <p class="review font-medium text-sm text-gray-700">{{ $member->review->title }}</p>
                                     @else
                                         <p class="font-medium text-sm text-gray-700">非公開</p>
                                     @endif
-                                </div>
+                                </div> -->
                             </a>
                         </div>
                     @endforeach
@@ -41,21 +40,21 @@
         document.addEventListener('DOMContentLoaded', () => {
             const introductions = document.querySelectorAll('.introduction');
             introductions.forEach((element) => {
-                const maxLength = 100;
+                const maxLength = 80;
                 const text = element.textContent.trim();
                 if (text.length >= maxLength) {
                     element.textContent = text.slice(0, maxLength-1) + '...';
                 }
             });
 
-            const reviews = document.querySelectorAll('.review');
-            reviews.forEach((element) => {
-                const maxLength = 21;
-                const text = element.textContent.trim();
-                if (text.length >= maxLength) {
-                    element.textContent = text.slice(0, maxLength-1) + '...';
-                }
-            });
+            // const reviews = document.querySelectorAll('.review');
+            // reviews.forEach((element) => {
+            //     const maxLength = 21;
+            //     const text = element.textContent.trim();
+            //     if (text.length >= maxLength) {
+            //         element.textContent = text.slice(0, maxLength-1) + '...';
+            //     }
+            // });
         });
     </script>
 </x-app-layout>
