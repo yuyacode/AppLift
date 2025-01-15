d9ca112ca099ca1d6bbe6c2325f536f323c96aa9
長塚裕也 <ny410144@gmail.com>
2025-01-15 12:03:38 +0900

    バリデーション項目名を追加

--- company/lang/ja/validation.php
+++ company/lang/ja/validation.php
@@ -256,12 +256,15 @@ return [
         'updated_at' => '更新日',
         'username' => 'ユーザー名',
         'year' => '年',
+
         'department' => '部署、部門',
         'occupation' => '職種、職業',
         'position' => '役職、ポジション',
         'join_date' => '入社時期',
         'introduction' => '自己紹介',
         'homepage' => 'ホームページ',
+        'answers.*.score' => '点数',
+        'answers.*.answer' => 'コメント',
     ],
 
 ];
