d6cd8688f9b3869fb44deb44ecf48a9cbfc5fc87
長塚裕也 <ny410144@gmail.com>
2025-02-02 02:15:15 +0900

    AuthenticationTestクラスを当プロジェクトに合わせて修正

--- student/database/factories/UserFactory.php
+++ student/database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
             'account_id' => (string) fake()->uuid(),
+            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
