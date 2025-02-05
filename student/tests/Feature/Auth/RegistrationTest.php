926b4cfc5217aebc80e784d8de83a2b83d5b1948
長塚裕也 <ny410144@gmail.com>
2025-02-05 11:26:59 +0900

    student.registrationTestクラスを本アプリに合わせて修正

--- student/tests/Feature/Auth/RegistrationTest.php
+++ student/tests/Feature/Auth/RegistrationTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature\Auth;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
+use Tests\Traits\RefreshMultipleDatabases;
 
 class RegistrationTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshMultipleDatabases;
 
     public function test_registration_screen_can_be_rendered(): void
     {
@@ -18,14 +19,29 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register(): void
     {
+        $fakeUrl = config('api.message_api_base_url_backend').'/register';
+
+        Http::fake([
+            $fakeUrl => Http::response([
+                'message' => 'OAuth registration was successful',
+                'detail'  => '',
+            ], 200),
+        ]);
+
         $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
+            'name'                  => 'Test User',
+            'email'                 => 'test@example.com',
+            'password'              => 'password',
             'password_confirmation' => 'password',
         ]);
 
         $this->assertAuthenticated();
         $response->assertRedirect(route('dashboard', absolute: false));
+
+        Http::assertSent(function ($request) use ($fakeUrl) {
+            return $request->url() === $fakeUrl
+                && $request['user_id'] === 1
+                && $request['app_kind'] === 'student';
+        });
     }
 }
