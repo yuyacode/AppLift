9afbbf47542bd3843afe1bb4b0e83b3d68afc404
長塚裕也 <ny410144@gmail.com>
2025-02-11 18:46:56 +0900

    ProfileTestクラスを本アプリに合わせて修正（学生側）

--- student/database/factories/UserFactory.php
+++ student/database/factories/UserFactory.php
@@ -24,12 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'account_id' => (string) fake()->uuid(),
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'account_id'        => (string) fake()->uuid(),
+            'name'              => fake()->name(),
+            'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password'          => static::$password ??= Hash::make('password'),
+            'remember_token'    => Str::random(10),
         ];
     }
 
