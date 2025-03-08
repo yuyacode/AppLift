c459e5ed1aac43dfb26fda41f7564e8d901ff748
長塚裕也 <ny410144@gmail.com>
2025-03-08 19:59:17 +0900

    学生側 > 企業情報 > 詳細画面表示機能に対する機能テストコード追加

--- student/tests/Feature/CompanyInfoTest.php
+++ student/tests/Feature/CompanyInfoTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\Models\CompanyInfo;
 use App\Models\CompanyInfoViewLog;
+use App\Models\CompanyUser;
+use App\Models\Review;
 use App\Models\User;
 use Tests\TestCase;
 use Tests\Traits\RefreshMultipleDatabases;
@@ -55,5 +57,78 @@ class CompanyInfoTest extends TestCase
             ->assertViewHas('recent_viewed_companies', function ($collection) {
                 return $collection->isEmpty();
             });
+    }
+
+    public function test_show_creates_new_view_log_and_displays_view()
+    {
+        $user = User::factory()->create();
+
+        $companyInfo = CompanyInfo::factory()->create();
+        $companyUser = CompanyUser::factory()->for($companyInfo)->create();
+        Review::factory()->for($companyUser)->create();
+
+        $existingLog = CompanyInfoViewLog::factory()->create([
+            'user_id'         => $user->id,
+            'company_info_id' => $companyInfo->id,
+        ]);
+
+        $response = $this
+            ->actingAs($user)
+            ->get(route('company_info.show', ['company_info' => $companyInfo->id]));
+
+        $response->assertOk()
+            ->assertViewIs('company_info.show')
+            ->assertViewHas('company_info', $companyInfo)
+            ->assertViewHas('members', function ($members) use ($companyUser) {
+                return $members->contains($companyUser) && $members->first()->relationLoaded('review');
+            });
+
+        $this->assertDatabaseMissing('company_info_view_logs', [
+            'id' => $existingLog->id,
+        ], 'student');
+
+        $this->assertDatabaseHas('company_info_view_logs', [
+            'user_id'         => $user->id,
+            'company_info_id' => $companyInfo->id,
+        ], 'student');
+    }
+
+    public function test_show_does_not_remove_any_record_if_user_has_less_than_10_logs()
+    {
+        $user = User::factory()->create();
+        $companyInfo = CompanyInfo::factory()->create();
+
+        $fakeCompanyInfoIds = [100, 200, 300];  // $companyInfo->id（1）は、delete対象となってしまい、最終的な件数の検証で失敗するため、$companyInfo->id（1）だけは避ける
+
+        foreach ($fakeCompanyInfoIds as $id) {
+            CompanyInfoViewLog::factory()->create([
+                'user_id'         => $user->id,
+                'company_info_id' => $id,
+            ]);
+        }
+
+        $response = $this
+            ->actingAs($user)
+            ->get(route('company_info.show', ['company_info' => $companyInfo->id]));
+
+        $response->assertOk()
+            ->assertViewIs('company_info.show');
+
+        foreach ($fakeCompanyInfoIds as $id) {
+            $this->assertDatabaseHas('company_info_view_logs', [
+                'user_id'         => $user->id,
+                'company_info_id' => $id,
+            ], 'student');
+        }
+
+        $this->assertCount(
+            4,  // 既存３件＋新規１件
+            CompanyInfoViewLog::where('user_id', $user->id)->get()
+        );
+
+        $this->assertDatabaseHas('company_info_view_logs', [
+            'user_id'         => $user->id,
+            'company_info_id' => $companyInfo->id,
+        ], 'student');
     }
 }
