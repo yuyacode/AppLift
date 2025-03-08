0b249d6688e8055fbfc144bdb54426e721ee0f1e
長塚裕也 <ny410144@gmail.com>
2025-03-08 18:06:15 +0900

    CompanyInfoFactoryとCompanyInfoViewLogFactoryを追加

--- student/app/Models/CompanyInfo.php
+++ student/app/Models/CompanyInfo.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CompanyInfo extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $connection = 'common';
 
